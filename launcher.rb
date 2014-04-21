4fae2eb6ad18f2195aa5d988df529d203f8b9d06
denzel <pressey90@gmail.com>
2014-04-20 22:02:10 -0400

    add three methods to class launcher

--- launcher.rb
+++ launcher.rb
@@ -4,8 +4,28 @@
 # this code loads a given file into an associated application
 
 class Launcher
+  def initialize(app_map)
+    @app_map = app_map
+  end
+
+  # execute the given file using the associate app
+  def run file_name
+    application = select_app file_name
+    system "#{application} #{file_name}"
+  end
+
+  # given a file, look up the matching application
+  def select_app file_name
+    ftype = file_type file_name
+    @app_map[ftype]
+  end
+
+  # return the part of the file name string after the last '.'
+  def file_type file_name
+    File.extname(file_name).gsub(/^\./, '').downcase
+  end
 end
 
-launcher = Launcher.new
+launcher = Launcher.new 
 
 
