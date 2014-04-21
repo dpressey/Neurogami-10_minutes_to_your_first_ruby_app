9f7a927ff8a55dbda2eeb97b3fdad185bfe1a69f
denzel <pressey90@gmail.com>
2014-04-20 21:30:39 -0400

    create a class and object. create a readme

---
+++ launcher.rb
@@ -0,0 +1,11 @@
+#! /usr/bin/env ruby
+
+# example application to demostrate some basic Ruby features
+# this code loads a given file into an associated application
+
+class Launcher
+end
+
+launcher = Launcher.new
+
+
